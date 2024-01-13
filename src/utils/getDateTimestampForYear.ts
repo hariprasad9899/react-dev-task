2888d14b13da14e3ee8815684e861c1e7d7b3f7f
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2024-01-13 22:16:41 +0530

    change variable name in helper function

--- src/utils/getDateTimestampForYear.ts
+++ src/utils/getDateTimestampForYear.ts
@@ -9,15 +9,14 @@ interface TimeStampForYear {
  * @returns {TimeStampForYear} - Object, that constains time stamp for Start and end
  */
 export function getDateTimestampForYear(year:number) : TimeStampForYear {
-    const januaryFirst = new Date(`${year}-01-01T00:00:00Z`);
-    const januaryFirstTimestamp = Math.floor(januaryFirst.getTime() / 1000);
+    const firstDayOfYear = new Date(`${year}-01-01T00:00:00Z`);
+    const startTimestamp = Math.floor(firstDayOfYear.getTime() / 1000);
 
-    // December 31st, YYYY 23:59:59 UTC
-    const decemberThirtyFirst = new Date(`${year}-12-31T23:59:59Z`);
-    const decemberThirtyFirstTimestamp = Math.floor(decemberThirtyFirst.getTime() / 1000);
+    const lastDayOfYear = new Date(`${year}-12-31T23:59:59Z`);
+    const endTimestamp = Math.floor(lastDayOfYear.getTime() / 1000);
 
     return {
-      startTimestamp: januaryFirstTimestamp,
-      endTimestamp: decemberThirtyFirstTimestamp
+      startTimestamp,
+      endTimestamp
     };
 }
